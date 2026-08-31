a00b2031c9674d230876afb8423de3b23ea5ea59
buildbylogic-netizen <buildbylogic@gmail.com>
2026-08-31 00:42:43 -0700

    Create EmployeeOperations.java

---
+++ EmployeeOperations.java
@@ -0,0 +1,12 @@
+package com.mycompany.smartemployeemanagementsystem.interfaces;
+
+public interface EmployeeOperations {
+
+    void addEmployee();
+
+    void removeEmployee();
+
+    void searchEmployee();
+
+    void updateEmployee();
+}
